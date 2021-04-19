685c328651196bf55b9568284c925f51c196534d
Juan Camilo Zorrilla <juanmiloz@hotmail.com>
2021-04-19 16:20:53 -0500

    add new class player, and implement the main class

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,10 +1,37 @@
 package gui;
 
-import java.util.ArrayList;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import model.DataManagement;
 
-public class Main {
+public class Main extends Application {
+
+	private DataManagementGUI dataManagementGUI;
+	private DataManagement dataManagement;
+
+	public Main(){
+		dataManagement = new DataManagement();
+		dataManagementGUI = new DataManagementGUI(dataManagement);
+	}
 
 	public static void main(String[] args) {
+		launch(args);
+	}
 
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("mainPane.fxml"));
+		fxmlLoader.setController(dataManagementGUI);
+		Parent root = fxmlLoader.load();
+
+		Scene scene = new Scene(root,1366,700);
+		dataManagementGUI.showMainScreen();
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Data Management");
+
+		primaryStage.show();
 	}
 }
---
+++ src/model/Player.java
@@ -0,0 +1,87 @@
+package model;
+
+public class Player {
+    private String name;
+    private String team;
+    private int year;
+    private double per;
+    private double trueShooting;
+    private double rebounds;
+    private double assists;
+    private double steals;
+
+    public Player(String name,String team, int year, double per, double trueShooting, double rebounds, double assists, double steals){
+        this.name = name;
+        this.team = team;
+        this.year = year;
+        this.per = per;
+        this.trueShooting = trueShooting;
+        this.rebounds = rebounds;
+        this.assists = assists;
+        this.steals = steals;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public double getPer() {
+        return per;
+    }
+
+    public void setPer(double per) {
+        this.per = per;
+    }
+
+    public double getTrueShooting() {
+        return trueShooting;
+    }
+
+    public void setTrueShooting(double trueShooting) {
+        this.trueShooting = trueShooting;
+    }
+
+    public double getRebounds() {
+        return rebounds;
+    }
+
+    public void setRebounds(double rebounds) {
+        this.rebounds = rebounds;
+    }
+
+    public double getAssists() {
+        return assists;
+    }
+
+    public void setAssists(double assists) {
+        this.assists = assists;
+    }
+
+    public double getSteals() {
+        return steals;
+    }
+
+    public void setSteals(double steals) {
+        this.steals = steals;
+    }
+}
