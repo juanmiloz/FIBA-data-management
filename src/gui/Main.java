14c569872527eb0d28c1db3d8e6e63b260385571
Juan Sanin <juanpablosaninb@gmail.com>
2021-04-18 22:19:23 -0500

    Functioning FXML first classes implemented

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,10 +1,44 @@
 package gui;
 
-public class Main {
+import java.io.FileInputStream;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import model.DataManagement;
+
+
+public class Main extends Application{
+
+	private DataManagementGUI dataManagementGUI;
+	private DataManagement dataManagement;
+	
+	public Main() {
+		dataManagement= new DataManagement();
+		dataManagementGUI = new DataManagementGUI(dataManagement);
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		launch(args);
 
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("mainPane.fxml"));
+		fxmlLoader.setController(dataManagementGUI);
+		primaryStage.getIcons().add(new Image(new FileInputStream("data/images/FIBA_logo.png")));
+		Parent root = fxmlLoader.load();
+
+		Scene scene = new Scene(root,1366,700);
+		dataManagementGUI.showMainScreen();
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Bookstore");
+		
+		primaryStage.show();
+		
 	}
 
 }
---
+++ src/model/DataManagement.java
@@ -0,0 +1,8 @@
+package model;
+
+public class DataManagement {
+
+	public DataManagement() {
+		
+	}
+}
